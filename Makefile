492f57ef3ff58d486f0f29675768570d87d801d4
Kin Yuunan <yuunan.kin@gmail.com>
2024-11-24 11:44:47 +0900

    initial

---
+++ Makefile
@@ -0,0 +1,5 @@
+sass: 
+	sass styles/index.scss styles/index.css --style compressed
+
+sass-watch:
+	sass --watch styles/index.scss:styles/index.css --style compressed
---
+++ js/index.js
@@ -0,0 +1,6 @@
+console.log(`Hello! Thanks for coming.`);
+console.log(
+  `If you like to talk to me, send me message at yuunan.kin@gmail.com.`
+);
+console.log(`I'm looking forward to hearing from you!`);
+console.log(`Have a nice day!`);
